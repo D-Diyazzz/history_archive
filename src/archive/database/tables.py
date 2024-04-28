5f0edd7747dd4ca2529555431a598ae6a89018a5
D-Diyazzz <diyazzz.dev@gmail.com>
2024-04-28 19:07:59 +0500

    fix: rewrite tables for documents and add types of documents

--- src/archive/database/tables.py
+++ src/archive/database/tables.py
@@ -73,25 +73,109 @@ collection = Table(
     Column("recommendations", Text, nullable=True),
 )
 
+# documents = Table(
+#     "documents",
+#     mapper_registry.metadata,
+#     Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
+#     Column("file_url", Text, nullable=False),
+#     Column("title", String(700), nullable=False),
+#     Column("heading", String(500), nullable=False),
+#     Column("author", String(500), nullable=False),
+#     Column("description_content", String(1000), nullable=False),
+#     Column("dating", String(255), nullable=False),
+#     Column("legends", Text, nullable=False),
+#     Column("format_doc", String(500), nullable=False),
+#     Column("color_palette", String(500), nullable=False),
+#     Column("resolution", String(500), nullable=False),
+#     Column("compression", String(500), nullable=False),
+#     Column("scanner_model", String(500), nullable=False),
+#     Column("created_at", DateTime(timezone=True), nullable=False)
+# )
+
 documents = Table(
     "documents",
     mapper_registry.metadata,
     Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
+    Column("search_data_id", BigInteger, ForeignKey("search_data.id"), nullable=False),
     Column("file_url", Text, nullable=False),
-    Column("title", String(700), nullable=False),
-    Column("heading", String(500), nullable=False),
     Column("author", String(500), nullable=False),
-    Column("description_content", String(1000), nullable=False),
     Column("dating", String(255), nullable=False),
-    Column("legends", Text, nullable=False),
-    Column("format_doc", String(500), nullable=False),
-    Column("color_palette", String(500), nullable=False),
-    Column("resolution", String(500), nullable=False),
-    Column("compression", String(500), nullable=False),
-    Column("scanner_model", String(500), nullable=False),
+    Column("place_of_creating", String(500), nullable=False),
+    Column("variety", String(255), nullable=False),
+    Column("addressee", String(500), nullable=False),
+    Column("brief_content", Text, nullable=False),
+    Column("case_prod_number", String(255), nullable=False),
+    Column("main_text", Text, nullable=False),
     Column("created_at", DateTime(timezone=True), nullable=False)
 )
 
+photo_documents = Table(
+    "photo_documents",
+    mapper_registry.metadata,
+    Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
+    Column("search_data_id", BigInteger, ForeignKey("search_data.id"), nullable=False),
+    Column("file_url", Text, nullable=False),
+    Column("author", String(500), nullable=False),
+    Column("dating", String(255), nullable=False),
+    Column("place_of_creating", String(500), nullable=False),
+    Column("title", String(500), nullable=False),
+    Column("completeness_of_reproduction", String(500), nullable=False),
+    Column("storage_media", String(255), nullable=False),
+    Column("color", String(255), nullable=False),
+    Column("size_of_original", String(255), nullable=False),
+    Column("image_scale", String(255), nullable=False),
+    Column("created_at", DateTime(timezone=True), nullable=False)
+)
+
+video_documents = Table(
+    "video_documents",
+    mapper_registry.metadata,
+    Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
+    Column("search_data_id", BigInteger, ForeignKey("search_data.id"), nullable=False),
+    Column("file_url", Text, nullable=False),
+    Column("author", String(500), nullable=False),
+    Column("dating", String(255), nullable=False),
+    Column("place_of_creating", String(500), nullable=False),
+    Column("title", String(500), nullable=False),
+    Column("volume", String(255), nullable=False),
+    Column("num_of_parts", String(255), nullable=False),
+    Column("color", String(255), nullable=False),
+    Column("creator", String(500), nullable=False),
+    Column("info_of_publication", Text, nullable=False),
+    Column("created_at", DateTime(timezone=True), nullable=False)
+)
+
+phono_documents = Table(
+    "phono_documents",
+    mapper_registry.metadata,
+    Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
+    Column("file_url", Text, nullable=False),
+    Column("author", String(500), nullable=False),
+    Column("dating", String(255), nullable=False),
+    Column("place_of_creating", String(500), nullable=False),
+    Column("genre", String(255), nullable=False),
+    Column("brief_summary", String(500), nullable=False),
+    Column("addressee", String(500), nullable=False),
+    Column("cypher", String(255), nullable=False),
+    Column("lang", String(255), nullable=False),
+    Column("storage_media", String(255), nullable=False),
+    Column("created_at", DateTime(timezone=True), nullable=False)
+)
+
+search_data = Table(
+    "search_data",
+    mapper_registry.metadata,
+    Column("id", BigInteger, primary_key=True, index=True, autoincrement=True),
+    Column("cypher", String(255), nullable=False),
+    Column("fund", String(255), nullable=False),
+    Column("inventory", String(255), nullable=False),
+    Column("case", String(255), nullable=False),
+    Column("leaf", String(255), nullable=False),
+    Column("authenticity", String(255), nullable=False),
+    Column("lang", String(255), nullable=False),
+    Column("playback_method", String(500), nullable=False),
+    Column("other", Text, nullable=True)
+)
 
 user = Table(
     "user",
