b3ca4b8f8056e6f459b829a3b95ad96466ca47e3
D-Diyazzz <diyazzz.dev@gmail.com>
2024-06-12 19:22:19 +0500

    add statements and converters for all types document

--- src/archive/repository/document/converter.py
+++ src/archive/repository/document/converter.py
@@ -1,6 +1,6 @@
 import json
 
-from src.archive.domains.document import Document, SearchData
+from src.archive.domains.document import Document, SearchData, PhotoDocument, VideoDocument, PhonoDocument
 
 
 def search_data_to_dict(model: Document):
@@ -37,6 +37,54 @@ def document_to_dict(model: Document):
     return model_dict
     # return {k: v for k, v in model_dict.items() if v is not None}
 
+def photo_document_to_dict(model):
+    return {
+        "file_urls": json.dumps(model.file_urls) if model.file_urls else None,
+        "author": model.author if model.author else None,
+        "dating": model.dating if model.dating else None,
+        "place_of_creating": model.place_of_creating if model.place_of_creating else None,
+        "title": model.title if model.title else None,
+        "completeness_of_reproduction": model.completeness_of_reproduction if model.completeness_of_reproduction else None,
+        "storage_media": model.storage_media if model.storage_media else None,
+        "color": model.color if model.color else None,
+        "size_of_original": model.size_of_original if model.size_of_original else None,
+        "image_scale": model.image_scale if model.image_scale else None,
+        "created_at": model.created_at if model.created_at else None,
+        "search_data_id": model.search_data.id if model.search_data and model.search_data.id else None,
+    }
+
+def video_document_to_dict(model):
+    return {
+        "file_urls": json.dumps(model.file_urls) if model.file_urls else None,
+        "author": model.author if model.author else None,
+        "dating": model.dating if model.dating else None,
+        "place_of_creating": model.place_of_creating if model.place_of_creating else None,
+        "title": model.title if model.title else None,
+        "volume": model.volume if model.volume else None,
+        "num_of_parts": model.num_of_parts if model.num_of_parts else None,
+        "color": model.color if model.color else None,
+        "creator": model.creator if model.creator else None,
+        "info_of_publication": model.info_of_publication if model.info_of_publication else None,
+        "created_at": model.created_at if model.created_at else None,
+        "search_data_id": model.search_data.id if model.search_data and model.search_data.id else None,
+    }
+
+def phono_document_to_dict(model):
+    return {
+        "file_urls": json.dumps(model.file_urls) if model.file_urls else None,
+        "author": model.author if model.author else None,
+        "dating": model.dating if model.dating else None,
+        "place_of_creating": model.place_of_creating if model.place_of_creating else None,
+        "genre": model.genre if model.genre else None,
+        "brief_summary": model.brief_summary if model.brief_summary else None,
+        "addressee": model.addressee if model.addressee else None,
+        "cypher": model.cypher if model.cypher else None,
+        "lang": model.lang if model.lang else None,
+        "storage_media": model.storage_media if model.storage_media else None,
+        "created_at": model.created_at if model.created_at else None
+    }
+
+
 
 def dict_to_document(data):
     search_data = SearchData(
@@ -66,3 +114,80 @@ def dict_to_document(data):
         created_at=data.created_at,
         search_data=search_data
     )
+
+def dict_to_photo_document(data):
+    search_data = SearchData(
+        id=data.search_data_id,
+        cypher=data.search_data_cypher,
+        fund=data.search_data_fund,
+        inventory=data.search_data_inventory,
+        case=data.search_data_case,
+        leaf=data.search_data_leaf,
+        authenticity=data.search_data_authenticity,
+        lang=data.search_data_lang,
+        playback_method=data.search_data_playback_method,
+        other=data.search_data_other
+    )
+
+    return PhotoDocument(
+        id=data.id,
+        file_urls=data.file_urls,
+        author=data.author,
+        dating=data.dating,
+        place_of_creating=data.place_of_creating,
+        title=data.title,
+        completeness_of_reproduction=data.completeness_of_reproduction,
+        storage_media=data.storage_media,
+        color=data.color,
+        size_of_original=data.size_of_original,
+        image_scale=data.image_scale,
+        created_at=data.created_at,
+        search_data=search_data
+    )
+
+def dict_to_video_document(data):
+    search_data = SearchData(
+        id=data.search_data_id,
+        cypher=data.search_data_cypher,
+        fund=data.search_data_fund,
+        inventory=data.search_data_inventory,
+        case=data.search_data_case,
+        leaf=data.search_data_leaf,
+        authenticity=data.search_data_authenticity,
+        lang=data.search_data_lang,
+        playback_method=data.search_data_playback_method,
+        other=data.search_data_other
+    )
+
+    return VideoDocument(
+        id=data.id,
+        file_urls=data.file_urls,
+        author=data.author,
+        dating=data.dating,
+        place_of_creating=data.place_of_creating,
+        title=data.title,
+        volume=data.volume,
+        num_of_parts=data.num_of_parts,
+        color=data.color,
+        creator=data.creator,
+        info_of_publication=data.info_of_publication,
+        created_at=data.created_at,
+        search_data=search_data
+    )
+
+def dict_to_phono_document(data):
+    return PhonoDocument(
+        id=data.id,
+        file_urls=data.file_urls,
+        author=data.author,
+        dating=data.dating,
+        place_of_creating=data.place_of_creating,
+        genre=data.genre,
+        brief_summary=data.brief_summary,
+        addressee=data.addressee,
+        cypher=data.cypher,
+        lang=data.lang,
+        storage_media=data.storage_media,
+        created_at=data.created_at
+    )
+
--- src/archive/repository/document/statements.py
+++ src/archive/repository/document/statements.py
@@ -128,7 +128,7 @@ update_search_data = text("""
         authenticity=:search_data_authenticity,
         lang=:search_data_lang,
         playback_method=:search_data_playback_method,
-        other=:search_data_other
+other=:search_data_other
     where id=:id
 
 """)
@@ -139,3 +139,268 @@ delete_search_data = text("""
     delete from search_data where id=:id
 
 """)
+
+
+insert_photo_document = text("""
+
+    insert into photo_documents (
+        search_data_id,
+        file_urls,
+        author,
+        dating,
+        place_of_creating,
+        title,
+        completeness_of_reproduction,
+        storage_media,
+        color,
+        size_of_original,
+        image_scale,
+        created_at
+    ) values (
+        :search_data_id,
+        :file_urls,
+        :author,
+        :dating,
+        :place_of_creating,
+        :title,
+        :completeness_of_reproduction,
+        :storage_media,
+        :color,
+        :size_of_original,
+        :image_scale,
+        :created_at
+    ) returning id
+
+""")
+
+select_photo_document_by_id = text("""
+
+    select 
+        pd.*,
+        s.id as search_data_id,
+        s.cypher as search_data_cypher,
+        s.fund as search_data_fund,
+        s.inventory as search_data_inventory,
+        s.case as search_data_case,
+        s.leaf as search_data_leaf,
+        s.authenticity as search_data_authenticity,
+        s.lang as search_data_lang,
+        s.playback_method as search_data_playback_method,
+        s.other as search_data_other
+    from photo_documents pd
+    join search_data s on pd.search_data_id = s.id
+    where pd.id=:id
+
+""")
+
+select_photo_document = text("""
+
+    select 
+        pd.*,
+        s.id as search_data_id,
+        s.cypher as search_data_cypher,
+        s.fund as search_data_fund,
+        s.inventory as search_data_inventory,
+        s.case as search_data_case,
+        s.leaf as search_data_leaf,
+        s.authenticity as search_data_authenticity,
+        s.lang as search_data_lang,
+        s.playback_method as search_data_playback_method,
+        s.other as search_data_other
+    from photo_documents pd
+    join search_data s on pd.search_data_id = s.id
+
+""")
+
+update_photo_document = text("""
+
+    update photo_documents
+    set
+        file_urls=:file_urls,
+        author=:author,
+        dating=:dating,
+        place_of_creating=:place_of_creating,
+        title=:title,
+        completeness_of_reproduction=:completeness_of_reproduction,
+        storage_media=:storage_media,
+        color=:color,
+        size_of_original=:size_of_original,
+        image_scale=:image_scale
+    where id=:id
+
+""")
+
+delete_photo_document = text("""
+
+    delete from photo_documents where id=:id
+
+""")
+
+
+insert_video_document = text("""
+
+    insert into video_documents (
+        search_data_id,
+        file_urls,
+        author,
+        dating,
+        place_of_creating,
+        title,
+        volume,
+        num_of_parts,
+        color,
+        creator,
+        info_of_publication,
+        created_at
+    ) values (
+        :search_data_id,
+        :file_urls,
+        :author,
+        :dating,
+        :place_of_creating,
+        :title,
+        :volume,
+        :num_of_parts,
+        :color,
+        :creator,
+        :info_of_publication,
+        :created_at
+    ) returning id
+
+""")
+
+select_video_document_by_id = text("""
+
+    select 
+        vd.*,
+        s.id as search_data_id,
+        s.cypher as search_data_cypher,
+        s.fund as search_data_fund,
+        s.inventory as search_data_inventory,
+        s.case as search_data_case,
+        s.leaf as search_data_leaf,
+        s.authenticity as search_data_authenticity,
+        s.lang as search_data_lang,
+        s.playback_method as search_data_playback_method,
+        s.other as search_data_other
+    from video_documents vd
+    join search_data s on vd.search_data_id = s.id
+    where vd.id=:id
+
+""")
+
+select_video_document = text("""
+
+    select 
+        vd.*,
+        s.id as search_data_id,
+        s.cypher as search_data_cypher,
+        s.fund as search_data_fund,
+        s.inventory as search_data_inventory,
+        s.case as search_data_case,
+        s.leaf as search_data_leaf,
+        s.authenticity as search_data_authenticity,
+        s.lang as search_data_lang,
+        s.playback_method as search_data_playback_method,
+        s.other as search_data_other
+    from video_documents vd
+    join search_data s on vd.search_data_id = s.id
+
+""")
+
+update_video_document = text("""
+
+    update video_documents
+    set
+        file_urls=:file_urls,
+        author=:author,
+        dating=:dating,
+        place_of_creating=:place_of_creating,
+        title=:title,
+        volume=:volume,
+        num_of_parts=:num_of_parts,
+        color=:color,
+        creator=:creator,
+        info_of_publication=:info_of_publication
+    where id=:id
+
+""")
+
+delete_video_document = text("""
+
+    delete from video_documents where id=:id
+
+""")
+
+
+insert_phono_document = text("""
+
+    insert into phono_documents (
+        file_urls,
+        author,
+        dating,
+        place_of_creating,
+        genre,
+        brief_summary,
+        addressee,
+        cypher,
+        lang,
+        storage_media,
+        created_at
+    ) values (
+        :file_urls,
+        :author,
+        :dating,
+        :place_of_creating,
+        :genre,
+        :brief_summary,
+        :addressee,
+        :cypher,
+        :lang,
+        :storage_media,
+        :created_at
+    ) returning id
+
+""")
+
+select_phono_document_by_id = text("""
+
+    select 
+        pd.*
+    from phono_documents pd
+    where pd.id=:id
+
+""")
+
+select_phono_document = text("""
+
+    select 
+        pd.*
+    from phono_documents pd
+
+""")
+
+update_phono_document = text("""
+
+    update phono_documents
+    set
+        file_urls=:file_urls,
+        author=:author,
+        dating=:dating,
+        place_of_creating=:place_of_creating,
+        genre=:genre,
+        brief_summary=:brief_summary,
+        addressee=:addressee,
+        cypher=:cypher,
+        lang=:lang,
+        storage_media=:storage_media
+    where id=:id
+
+""")
+
+delete_phono_document = text("""
+
+    delete from phono_documents where id=:id
+
+""")
+
